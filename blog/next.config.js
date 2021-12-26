e374391546c2f3bf0939e083b13557a7ea154935
CodyCline <20631166+CodyCline@users.noreply.github.com>
2021-12-26 12:33:06 -0800

    feat: Copy site metadata over, start commiting public folder

--- blog/next.config.js
+++ blog/next.config.js
@@ -2,3 +2,20 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+
+export const metadata = {
+  title: `Cody Cline`,
+  author: {
+    name: `Cody Cline`,
+    summary: `Software Developer based in Seattle, WA. 
+			My few areas of specialization are DevOps, security, and web app development. 
+			Currently, on a mission to craft useful, high-quality open-source software and patch security holes in the web.`,
+  },
+  description: `Developer blog and portfolio with code-focused content.`,
+  social: {
+    github: `codycline`
+  },
+  siteUrl: `https://codycline.com`
+}
