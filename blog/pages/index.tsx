d6729135c1dc820d185dc73100b9d10f2956fb52
CodyCline <20631166+CodyCline@users.noreply.github.com>
2021-12-28 14:22:04 -0800

    feat: Add functioning toggle component, add styled components

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,16 +1,26 @@
 import { getAllPosts } from "../lib/loadBlog";
 
+import dynamic from "next/dynamic";
+import styled from "styled-components";
+import { Navbar } from "../components/Navbar";
+
+const ThemeToggle:any = dynamic(():any => import("../components/ThemeToggle"), {
+  ssr: false,
+});
 
 function Home({posts}:any) { 
     console.log(posts);
     return (
         <div>
+        <Navbar>Hello</Navbar>
+
             <h1>Hello</h1>
             <ul>
                 <li>
                     posts
                 </li>
             </ul>
+            <ThemeToggle />
         </div>
     )
 }
